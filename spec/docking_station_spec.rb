6a3cbf73205a98f8d6ce49e1cb05d80e46e87164
Nathan Hynes <nathanmail@hotmail.co.uk>
2019-09-17 16:10:21 +0100

    Added test to DockingStation_spec to test for Bike class and whether it will respond to working?

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,4 +2,6 @@ require 'Docking_Station'
 
 describe DockingStation do
   it { is_expected.to respond_to(:release_bike) }
+  it { expect(DockingStation.new.release_bike.class).to eq Bike }
+  it {expect(DockingStation.new.release_bike).to respond_to (:working?)}
 end
