11ce9896d2d09d3ea72d4e4157508d79a847484e
kevindhanna <kevindhanna@gmail.com>
2019-09-19 17:10:48 +0100

    refactored tests in docking_station_spec

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,10 +1,9 @@
 require 'Docking_Station'
 
 describe DockingStation do
+  let(:bike) { double('bike') }
   describe '#initialize' do
     it 'can be initialized with a capacity' do
-      # expect(DockingStation).to receive(:new).with(1)
-      # DockingStation.new(1)
       expect(DockingStation.new(20))
     end
   end
@@ -12,41 +11,42 @@ describe DockingStation do
   describe '#release_bike' do
     # Test whether subject responds to release_bike method
     it { expect(DockingStation.new).to respond_to(:release_bike) }
-
+    
     # Test whether release_bike creates a Bike Class
     it 'creates a class of Bike' do
-      bike = double(:bike)
+      allow(bike).to receive(:working?) { true }
+      allow(bike).to receive(:class).and_return(Bike)
       subject.dock(bike)
       expect(subject.release_bike.class).to eq Bike
     end
 
     # Test whether the Bike release_bike creates, responds to working? method
     it 'releases a working bike' do
-      bike = double(:bike)
+      allow(bike).to receive(:working?) { true }
       subject.dock(bike)
       expect(subject.release_bike).to be_working
     end
-
+    
     # Test whether an error message is raised if there is no bikes.
     it 'returns error message if there are no bikes' do
       expect { subject.release_bike }.to raise_error('No Bike Stored')
     end
-
+    
     it 'doesnt release broken bikes' do
-      bike = double(:bike)
+      allow(bike).to receive(:report_broken) { true }
+      allow(bike).to receive(:working?).and_return(false)
       bike.report_broken
       subject.dock(bike)
       expect { subject.release_bike }.to raise_error('No working Bike Stored')
     end
   end
-
+  
   describe '#dock' do
     # Test to make sure DockingStation Class responds to Dock with 1 argument
     it { is_expected.to respond_to(:dock).with(1).argument }
 
     # Test to make sure docked bike returns Bike
     it 'returns docked bikes' do
-      bike = double(:bike)
       subject.dock(bike)
       expect(subject.docked_bikes[0]).to eq bike
     end
@@ -54,23 +54,23 @@ describe DockingStation do
     it 'has a default capacity limit' do
       # Fill the Station
       DockingStation::DEFAULT_CAPACITY.times do
-        subject.dock(double(:bike))
+        subject.dock(bike)
       end
       # Add one more bike
-      expect { subject.dock(double(:bike)) }.to raise_error 'No more space'
+      expect { subject.dock(bike) }.to raise_error 'No more space'
     end
     it 'can have a specified limit' do
       subject = DockingStation.new(30)
       # Fill the Station
       30.times do
-        subject.dock(double(:bike))
+        subject.dock(bike)
       end
       # Add one more bike
-      expect { subject.dock(double(:bike)) }.to raise_error 'No more space'
+      expect { subject.dock(bike) }.to raise_error 'No more space'
     end
-
+    
     it 'docks broken bikes' do
-      bike = double(:bike)
+      allow(bike).to receive(:report_broken) { true }
       bike.report_broken
       subject.dock(bike)
       expect(subject.docked_bikes[0]).to eq bike
