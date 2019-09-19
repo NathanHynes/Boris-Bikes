9e3b651614ed462fef3131bc13fd523ac3c5c0ae
Nathan Hynes <nathanmail@hotmail.co.uk>
2019-09-19 14:37:23 +0100

    refactored to abide by SRP principles

--- lib/Docking_Station.rb
+++ lib/Docking_Station.rb
@@ -9,14 +9,24 @@ class DockingStation
   end
 
   def release_bike
-    raise 'No Bike Stored' if @docked_bikes.empty?
+    raise 'No Bike Stored' if empty?
 
     @docked_bikes.pop
   end
 
   def dock(bike)
-    raise 'No more space' if @docked_bikes.length >= 20
+    raise 'No more space' if full?
 
     @docked_bikes << bike
+  end
+
+  private
+
+  def full?
+    true if @docked_bikes.length >= 20
+  end
+
+  def empty?
+    @docked_bikes.empty?
   end
 end
